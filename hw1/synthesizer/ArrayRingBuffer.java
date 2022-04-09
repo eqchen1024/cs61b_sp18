d70813ecaada39bf4489a5bc3d5f37ec065d833c
eqchen1024 <cdysbt@gmail.com>
2022-04-09 22:38:15 +0800

    HW1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,10 @@
 // TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +21,11 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        this.capacity = capacity;
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        rb = (T[]) new Object[this.capacity];
     }
 
     /**
@@ -30,6 +35,13 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (this.fillCount < this.capacity) {
+            last = (last + 1) % this.capacity;
+            rb[last] = x;
+            this.fillCount += 1;
+        } else {
+            throw new RuntimeException("Ring buffer overflow");
+        }
     }
 
     /**
@@ -39,6 +51,40 @@ public class ArrayRingBuffer<T>  {
      */
     public T dequeue() {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        if (this.fillCount != 0) {
+            T oldest = rb[first];
+            first = (first + 1) % this.capacity;
+            this.fillCount -= 1;
+            return oldest;
+        } else {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+    }
+
+    @Override
+    public int capacity() {
+        return this.capacity;
+    }
+
+    @Override
+    public int fillCount() {
+        return this.fillCount;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (this.fillCount == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isFull() {
+        if (this.fillCount == this.capacity) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -46,7 +92,31 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
+    @Override
+    public Iterator<T> iterator() {
+        return new myIterator();
+    }
+    private class myIterator implements Iterator<T> {
+        int cnt = 0;
+        @Override
+        public boolean hasNext() {
+            if (cnt < fillCount()) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public T next(){
+            if (hasNext()){
+                cnt += 1;
+                return rb[(first+cnt)% capacity()];
+            }
+            throw new RuntimeException("No more element");
+        }
+    }
 }
---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,22 @@
+import java.util.Iterator;
+
+public interface BoundedQueue<T> extends Iterable<T>{
+    public int capacity();     // return size of the buffer
+    public int fillCount();    // return number of items currently in the buffer
+    public void enqueue(T x);  // add item x to the end
+    public T dequeue();        // delete and return item from the front
+    public T peek();           // return (but do not delete) item from the front
+    public default boolean isEmpty() {
+        if (fillCount() == 0) {
+            return true;
+        }
+        return false;
+    }     // is the buffer empty (fillCount equals zero)?
+    public default boolean isFull() {
+        if (capacity() == fillCount()) {
+            return true;
+        }
+        return false;
+    }        // is the buffer full (fillCount is same as capacity)?
+    Iterator<T> iterator();
+}
