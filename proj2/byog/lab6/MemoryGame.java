a563c81610455c07239dda1211b78d2bc8e52295
eqchen1024 <cdysbt@gmail.com>
2022-05-01 01:58:22 +0800

    lab6 randString generated

--- proj2/byog/lab6/MemoryGame.java
+++ proj2/byog/lab6/MemoryGame.java
@@ -1,5 +1,6 @@
 package byog.lab6;
 
+import byog.Core.RandomUtils;
 import edu.princeton.cs.introcs.StdDraw;
 
 import java.awt.Color;
@@ -25,11 +26,14 @@ public class MemoryGame {
         }
 
         int seed = Integer.parseInt(args[0]);
-        MemoryGame game = new MemoryGame(40, 40);
-        game.startGame();
+        MemoryGame game = new MemoryGame(40, 40, seed);
+        String a = game.generateRandomString(5);
+        System.out.println(a);
+//        game.startGame();
+
     }
 
-    public MemoryGame(int width, int height) {
+    public MemoryGame(int width, int height, long seed) {
         /* Sets up StdDraw so that it has a width by height grid of 16 by 16 squares as its canvas
          * Also sets up the scale so the top left is (0,0) and the bottom right is (width, height)
          */
@@ -44,16 +48,25 @@ public class MemoryGame {
         StdDraw.enableDoubleBuffering();
 
         //TODO: Initialize random number generator
+        rand = new Random(seed);
+
     }
 
     public String generateRandomString(int n) {
         //TODO: Generate random string of letters of length n
-        return null;
+        String randomString = "";
+        for (int i = 0; i < n; i++) {
+            int idx = RandomUtils.uniform(rand, 26);
+            char charToShow = CHARACTERS[idx];
+            randomString = randomString + charToShow;
+        }
+        return randomString;
     }
 
     public void drawFrame(String s) {
         //TODO: Take the string and display it in the center of the screen
         //TODO: If game is not over, display relevant game information at the top of the screen
+
     }
 
     public void flashSequence(String letters) {
