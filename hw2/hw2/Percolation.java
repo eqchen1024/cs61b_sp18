74d8f3215b581c989bf5208d444a6187080f57b2
eqchen1024 <cdysbt@gmail.com>
2022-05-19 00:38:31 +0800

    hw2 v1 finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -9,31 +9,44 @@ public class Percolation {
     private int sideLength;
     private WeightedQuickUnionUF joinStatusSet;
     private int[] openStatusArray;
+    private int openCellNum = 0;
+
     public Percolation(int N) {
         sideLength = N;
-        joinStatusSet =  new WeightedQuickUnionUF(sideLength ^ 2);
-        openStatusArray = new int[sideLength ^ 2];
+        joinStatusSet =  new WeightedQuickUnionUF(sideLength * sideLength);
+        openStatusArray = new int[sideLength * sideLength];
     }
     // open the site (row, col) if it is not open already
     public void open(int row, int col) {
         int cellIndex = sideLength * row + col;
         openStatusArray[cellIndex] = 1;
+        openCellNum += 1;
+        List<Integer> neighbours= getNeighborIndex(row, col);
+        for (int neighbour: neighbours) {
+            if (openStatusArray[neighbour] == 1) {
+                joinStatusSet.union(neighbour, cellIndex);
+            }
+        }
     }
-    private int[] getNeighborIndex(int row, int col){
+
+    private List<Integer> getNeighborIndex(int row, int col){
         int cellIndex = sideLength * row + col;
         List<Integer> neighbourIndex = new ArrayList<Integer>();
-        int up, down, left, right;
-//        if (row == 0 && col == 0) {
-//            right = cellIndex + 1;
-//            down  = cellIndex + sideLength;
-//        } else if (row == sideLength -1 && col == sideLength -1) {
-//            left = cellIndex - 1;
-//            up  = cellIndex - sideLength;
-//        } else if (row == 0 && col == sideLength -1) {
-//            left = cellIndex - 1;
-//            up  = cellIndex - sideLength;
-//
-//    }
+        if (row != 0) {
+            neighbourIndex.add(cellIndex - sideLength);
+        }
+        if (col !=0) {
+            neighbourIndex.add(cellIndex - 1);
+        }
+        if (row != sideLength - 1) {
+            neighbourIndex.add(cellIndex + sideLength);
+        }
+        if (col != sideLength - 1) {
+            neighbourIndex.add(cellIndex + 1);
+        }
+        return neighbourIndex;
+    }
+
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         int cellIndex = sideLength * row + col;
@@ -43,8 +56,49 @@ public class Percolation {
             return false;
         }
     }
-    public boolean isFull(int row, int col)  // is the site (row, col) full?
-    public int numberOfOpenSites()           // number of open sites
-    public boolean percolates()              // does the system percolate?
-    public static void main(String[] args)   // use for unit testing (not required)
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        boolean full_flag = false;
+        int cellIndex = sideLength * row + col;
+        for (int i = 0; i < sideLength; i++) {
+            if (joinStatusSet.connected(i,cellIndex) && isOpen(row,col)) {
+                full_flag = true;
+            }
+        }
+        return full_flag;
+    }
+
+    // number of open sites
+    public int numberOfOpenSites(){
+        return openCellNum;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        boolean percolates_flag = false;
+        for (int i = 0; i < sideLength; i++) {
+            if(isFull(sideLength - 1, i)){
+                percolates_flag = true;
+            }
+        }
+        return percolates_flag;
+    }
+
+    // use for unit testing (not required)
+    public static void main(String[] args) {
+        Percolation a = new Percolation(10);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(a.isOpen(i,5));
+            a.isFull(i,5);
+            System.out.println(a.isFull(i,5));
+            a.open(i,5);
+            System.out.println(a.isOpen(i,5));
+            System.out.println(a.isFull(i,5));
+            a.percolates();
+            System.out.println(a.percolates());
+
+        }
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,65 @@
 package hw2;
 
-public class PercolationStats {
+import java.util.Random;
+import edu.princeton.cs.introcs.StdRandom;
 
+public class PercolationStats {
+    Random random = new Random();
+    Percolation p;
+    int row, col;
+    int[] threshRecords;
+    int thresh;
+    int sideLength;
+    int expTime;
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0){
+            throw new java.lang.IllegalArgumentException();
+        }
+        sideLength = N;
+        expTime = T;
+        p = pf.make(sideLength);
+        threshRecords = new int[expTime];
+        for (int expNum = 0; expNum < expTime; expNum ++) {
+            thresh = 0;
+            while (!p.percolates()) {
+                row = StdRandom.uniform(10);
+                col = StdRandom.uniform(10);
+                if (!p.isOpen(row,col)) {
+                    p.open(row,col);
+                    thresh += 1;
+                }
+            }
+            threshRecords[expNum] = thresh;
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        int sumOfThresh = 0;
+        for (int i =0; i < threshRecords.length; i++) {
+            sumOfThresh += threshRecords[i];
+        }
+        return sumOfThresh/expTime;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        double totalDeviationSquare = 0;
+        double variance;
+        double meanThresh = mean();
+        for (int i =0; i < threshRecords.length; i++) {
+            totalDeviationSquare += (threshRecords[i] - meanThresh) * (threshRecords[i] - meanThresh);
+        }
+        variance = totalDeviationSquare/(expTime -1);
+        return Math.sqrt(variance);
+    }
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev()/(Math.sqrt(expTime));
+    }
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev()/(Math.sqrt(expTime));
+    }
 }
