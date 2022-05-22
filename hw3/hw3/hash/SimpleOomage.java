2ba9c43cddbc11bad58576f7ed93c574304917ea
eqchen1024 <cdysbt@gmail.com>
2022-05-22 15:46:34 +0800

    hw finished except complex hash code

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -10,26 +10,39 @@ public class SimpleOomage implements Oomage {
     protected int blue;
 
     private static final double WIDTH = 0.01;
-    private static final boolean USE_PERFECT_HASH = false;
+    private static final boolean USE_PERFECT_HASH = true;
 
     @Override
     public boolean equals(Object o) {
-        // TODO: Write this method.
-        return false;
+        if (o == this) {
+            return true;
+        }
+        if (o == null){
+            return false;
+        }
+        if (o.getClass() != SimpleOomage.class) {
+            return false;
+        }
+        SimpleOomage o1 = (SimpleOomage) o;
+        if (red == o1.red && green == o1.green && blue == o1.blue) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
-    /* Uncomment this method after you've written
-       equals and failed the testHashCodeAndEqualsConsistency
-       test.
+
     @Override
     public int hashCode() {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
-            // TODO: Write a perfect hash function for Simple Oomages.
-            return 0;
+            return  (int) ( red/5 * Math.pow(Math.ceil(256/5),2) +
+                    green/5 * Math.pow(Math.ceil(256/5),1) +
+                    blue/5 * Math.pow(Math.ceil(256/5),0));
         }
-    }*/
+    }
 
     public SimpleOomage(int r, int g, int b) {
         if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
