f3208fdd2b4118c63b5171814517b5402e9c6aac
eqchen1024 <cdysbt@gmail.com>
2022-12-14 16:54:12 +0800

    CountingSort Finished

--- lab13/CountingSort.java
+++ lab13/CountingSort.java
@@ -67,6 +67,51 @@ public class CountingSort {
      */
     public static int[] betterCountingSort(int[] arr) {
         // TODO make counting sort work with arrays containing negative numbers.
-        return null;
+        int negativeCount = 0;
+        for (int num : arr) {
+            if (num < 0) {
+                negativeCount += 1;
+            }
+        }
+        int[] nonNegativeArr = new int[arr.length - negativeCount];
+        int[] negativeArr = new int[negativeCount];
+
+        int posPr = 0;
+        int negPr = 0;
+        for (int num : arr) {
+            if (num < 0) {
+                negativeArr[negPr] =num;
+                negPr += 1;
+            } else {
+                nonNegativeArr[posPr] =num;
+                posPr += 1;
+            }
+        }
+
+        for (int i = 0; i < negativeCount; i++) {
+            negativeArr[i] = - negativeArr[i];
+        }
+
+
+        int[] posSortedArr = naiveCountingSort(nonNegativeArr);
+
+        if (negativeArr.length == 0) {
+            return posSortedArr;
+        }
+
+        int [] negSortedArr = naiveCountingSort(negativeArr);
+        for (int j = 0; j < negativeCount; j++) {
+            negSortedArr[j] = - negSortedArr[j];
+        }
+
+        int[] res = new int[arr.length];
+        for (int k = 0; k < negSortedArr.length; k++) {
+            res[k] = negSortedArr[negSortedArr.length - k - 1];
+        }
+
+        for (int l = 0; l < posSortedArr.length; l++) {
+            res[l + negSortedArr.length] = posSortedArr[l];
+        }
+        return res;
     }
 }
